a2710db55bbe2498875b514674f5c9ac485119e0
Kenneth Marut <kenmarut@kenneths-mini.lan>
2022-09-07 08:55:14 -0500

    add averages

--- src/components/scorekeeper/ScoreKeeper.tsx
+++ src/components/scorekeeper/ScoreKeeper.tsx
@@ -9,12 +9,28 @@ export const ScoreKeeper: React.FC<IScoreKeeperProps> = ({
   numFail,
   numSuccess,
 }) => {
+    const average = (numSuccess / (numSuccess + numFail));
+    const displayAverage = isNaN(average) ? 0 : average;
   return (
     <div className="score-keeper-component card-wrapper">
-      <h2>Score</h2>
-      <div className="number-wrapper">
-      <h2 className="success">{numSuccess}</h2>
-      <h2 className="fail">{numFail}</h2>
+      <h2 className="score-title">Stats</h2>
+      <div className="stat-container">
+        <div className="number-wrapper">
+          <div className="score-display correct-container">
+            <h2 className="success">{numSuccess}</h2>
+            <h2 className="label-for-success">Correct</h2>
+          </div>
+          <div className="score-display incorrect-container">
+            <h2 className="fail">{numFail}</h2>
+            <h2 className="label-for-fail">Incorrect</h2>
+          </div>
+          <div className="score-display average-container">
+            <h2 className="score-display average">
+              {`${displayAverage}%`}
+            </h2>
+            <h2 className="label-for-average">Average</h2>
+          </div>
+        </div>
       </div>
     </div>
   );
