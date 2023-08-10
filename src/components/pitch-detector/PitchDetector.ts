bea9a4d837ee40064440f25978ceb3ba1863f3c9
Kenneth Marut <kenmarut@kenneths-mini.lan>
2023-08-10 16:43:34 -0500

    primitive pitch detector

--- src/components/pitch-detector/PitchDetector.ts
+++ src/components/pitch-detector/PitchDetector.ts
@@ -4,6 +4,24 @@ import { Input, WebMidi, Listener } from "webmidi";
 import { PitchDetector as Pitchy } from "pitchy";
 import { IController } from "../../constants";
 
+const NOTES = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+const A4_FREQ = 440; // Frequency of note A4
+// const TWELFTH_ROOT_OF_TWO = Math.pow(2, 1 / 12);
+
+function frequencyToNote(frequency: number): string {
+  // Calculate how many half-steps away the frequency is from A4
+  const halfStepsFromA4 = Math.round(12 * Math.log2(frequency / A4_FREQ));
+
+  // Calculate the absolute note number where 0 is A0
+  const absoluteNoteNumber = 57 + halfStepsFromA4;
+
+  // Convert absolute note number to note and octave
+  const note = NOTES[absoluteNoteNumber % 12];
+  const octave = Math.floor(absoluteNoteNumber / 12);
+
+  return note + octave.toString();
+}
+
 export class PitchDetector implements IController {
   protected midiAccess: MIDIAccess | undefined;
   readonly onKeyEmitter = new EventEmitter();
@@ -12,6 +30,8 @@ export class PitchDetector implements IController {
   protected noteOnListener: Listener | undefined;
   protected noteOffListener: Listener | undefined;
   protected audioContext: AudioContext | undefined;
+
+  protected onNotes = new Set<string>();
 
   async initialize(): Promise<void> {
     this.audioContext = new window.AudioContext();
@@ -24,12 +44,17 @@ export class PitchDetector implements IController {
     this.audioContext.createMediaStreamSource(userMedia).connect(analayzerNode);
     const detector = Pitchy.forFloat32Array(analayzerNode.fftSize);
     const input = new Float32Array(detector.inputLength);
-    this.updatePitch(analayzerNode, detector, input, this.audioContext.sampleRate);
+    this.updatePitch(
+      analayzerNode,
+      detector,
+      input,
+      this.audioContext.sampleRate
+    );
     return Promise.resolve(undefined);
   }
 
   resumeAudioContext(): void {
-    console.log('do we have audio context', this.audioContext);
+    console.log("do we have audio context", this.audioContext);
     this.audioContext?.resume();
   }
 
@@ -41,9 +66,18 @@ export class PitchDetector implements IController {
   ) {
     analyserNode.getFloatTimeDomainData(input);
     const [pitch, clarity] = detector.findPitch(input, sampleRate);
+    const roundedPitch = Math.round(pitch * 10) / 10;
+    const clarityPercent = Math.round(clarity * 100);
+    console.log(clarityPercent);
+    if (clarityPercent > 85) {
+      const noteName = frequencyToNote(roundedPitch);
+      console.log("Note", noteName);
+      this.fireNoteOn(noteName);
+    } else {
+      this.turnOffNotes();
+    }
+    // console.log("Pitch", `${Math.round(pitch * 10) / 10} Hz`);
 
-    console.log("Pitch", `${Math.round(pitch * 10) / 10} Hz`);
-    console.log("Clarity", `${Math.round(clarity * 100)} %`);
     window.setTimeout(
       () => this.updatePitch(analyserNode, detector, input, sampleRate),
       100
@@ -51,8 +85,16 @@ export class PitchDetector implements IController {
   }
 
   protected fireNoteOn = (note: string): void => {
+    this.onNotes.add(note);
     this.onKeyEmitter.emit("noteon", note);
   };
+
+  protected turnOffNotes = () => {
+    Array.from(this.onNotes).forEach(note => {
+      this.fireNoteOff(note);
+    })
+  };
+
   protected fireNoteOff = (note: string): void => {
     this.onKeyEmitter.emit("noteoff", note);
   };
